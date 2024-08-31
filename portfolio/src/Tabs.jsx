c915cb8bf4c21093e2f4ac8a35a1133eb3b28e4a
Abhinav Singirthi <abhinav_singirthi@Abhinavs-MacBook-Air.local>
2024-08-31 23:19:16 +0530

    Mobile friendly

--- portfolio/src/Tabs.jsx
+++ portfolio/src/Tabs.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './Tabs.css';
 
-const Tabs = ({ onChange }) => {
+const Tabs = ({ onChange, activeTab }) => {
   return (
     <div className="tabs">
-      <div className="tab" onClick={() => onChange('all')}>All</div>
-      <div className="tab" onClick={() => onChange('about')}>About</div>
-      <div className="tab" onClick={() => onChange('work')}>Work</div>
+      <div 
+        className={`tab ${activeTab === 'all' ? 'active' : ''}`} 
+        onClick={() => onChange('all')}
+      >
+        All
+      </div>
+      <div 
+        className={`tab ${activeTab === 'about' ? 'active' : ''}`} 
+        onClick={() => onChange('about')}
+      >
+        About
+      </div>
+      <div 
+        className={`tab ${activeTab === 'work' ? 'active' : ''}`} 
+        onClick={() => onChange('work')}
+      >
+        Work
+      </div>
     </div>
   );
 };
